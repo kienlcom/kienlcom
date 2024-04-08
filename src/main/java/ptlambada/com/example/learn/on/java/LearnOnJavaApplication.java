cdb06f56b9dc29c91423a278c339df241c06d9af
Phuoc Tran <phuoc.tranvan.97@gmail.com>
2024-04-08 23:57:12 +0700

    First commit

---
+++ src/main/java/ptlambada/com/example/learn/on/java/LearnOnJavaApplication.java
@@ -0,0 +1,13 @@
+package ptlambada.com.example.learn.on.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnOnJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnOnJavaApplication.class, args);
+	}
+
+}
